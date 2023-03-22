7358f10b03a6af1876e0765a653cb02fc74dea31
GhostClock <zhangyuanlaifen@gmail.com>
2023-03-22 20:54:01 +0800

    完成Home页面的静态组件拆分

--- app/src/pages/Home/index.vue
+++ app/src/pages/Home/index.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
-    我是首页
+    <!-- 三级联动的全局组件：已经注册为全局组件，不需要再次引入 -->
+    <TypeNav />
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Floor />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
 <script>
-    export default {
+// 引入其余的组件
+import ListContainer from './ListContainer';
+import Recommend from './Recommend';
+import Rank from './Rank';
+import Like from './Like';
+import Floor from './Floor';
+import Brand from './Brand';
 
-    }
+export default {
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+  }
+}
 </script>
 
 <style>
