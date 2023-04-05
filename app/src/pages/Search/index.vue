c8af3984f06b74ccd194da0f5a90ecd08dc6aed1
GhostClock <zhangyuanlaifen@gmail.com>
2023-04-05 16:31:41 +0800

    使用声明式导航跳转到商品详情页

--- app/src/pages/Search/index.vue
+++ app/src/pages/Search/index.vue
@@ -55,9 +55,10 @@
               <li class="yui3-u-1-5" v-for="good in goodsList" :key="good.id">
                 <div class="list-wrap">
                   <div class="p-img">
-                    <a href="item.html" target="_blank">
+                    <!-- 声明式路由跳转时 需要带上id参数 params-->
+                    <router-link :to="`/detail/${good.id}`">
                       <img :src="good.defaultImg" />
-                    </a>
+                    </router-link>
                   </div>
                   <div class="price">
                     <strong>
