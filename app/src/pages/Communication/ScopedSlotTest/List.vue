643f355d136cb83167f308f3767c548190c7574e
GhostClock <zhangyuanlaifen@gmail.com>
2023-05-09 00:50:04 +0800

    插槽：作用域插槽的使用

---
+++ app/src/pages/Communication/ScopedSlotTest/List.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <li v-for="item in todos" :key="item.id"> 
+            <!-- 作用域插槽 作用域插槽的语法规则 -->
+            <slot :todo="item"></slot>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    props: {
+        todos: Array,
+        default: []
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ app/src/pages/Communication/ScopedSlotTest/List1.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in todos" :key="item.id">
+      <!-- 作用域插槽 作用域插槽的语法规则 -->
+      <slot :todo="item" :index="index"></slot>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props: {
+    todos: Array,
+    default: [],
+  },
+};
+</script>
+<style lang="">
+</style>
